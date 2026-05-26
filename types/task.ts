fb42062a370b67ed4090bedbee7e5d64720bd809
WuangHuyyy <75169577+emsiqh@users.noreply.github.com>
2026-05-26 16:06:22 +0700

    Add Task types

---
+++ types/task.ts
@@ -0,0 +1,18 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CreateTaskInput {
+  title: string;
+  description: string;
+}
+
+export interface UpdateTaskInput {
+  title?: string;
+  description?: string;
+}
